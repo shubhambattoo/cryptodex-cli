5a3924891e536a0dfc40eafbad1a80cb021ecbad
shubhambattoo <shubham.battoo@hotmail.com>
2020-07-12 13:35:23 +0530

    show coin prices

--- bin/cryptodex.js
+++ bin/cryptodex.js
@@ -5,6 +5,6 @@ const pkg = require('./../package.json');
 program
   .version(pkg.version)
   .command('key', 'Manage API key -- https://nomics.com')
+  .command('check', 'Check Coin Price Info')
   .parse(process.argv);
 
-console.log('Hello world');
